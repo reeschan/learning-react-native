9843199330ea72a5bf246257dc3de0e0f672a8cb
Ryuki Tobita <71293400+reeschan@users.noreply.github.com>
2025-03-20 23:13:37 +0900

    Capacitor の設定ファイルを追加

---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+const config = {
+  appId: 'com.example.myfirstapp',
+  appName: 'My First App',
+  webDir: 'build',
+  bundledWebRuntime: false,
+  ios: {
+    contentInset: 'always',
+  }
+};
+
+export default config;
